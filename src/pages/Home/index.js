3e10598772a1e926ae4afd91e48f80ec8b9405d3
Carlos Alonzo Lagos <calonzolg@gmail.com>
2020-08-09 13:48:55 -0600

    retrieve all countries from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import useSWR from "swr";
+import React, { useState, useEffect } from "react";
 import {
   Accordion,
   AccordionItem,
@@ -10,54 +9,67 @@ import {
   Grid,
   Text,
   Spinner,
+  Stack,
 } from "@chakra-ui/core";
+import SearchInput from "../../components/SearchInput";
+import { formatNumber } from "../../utils";
 
 const url = `${process.env.REACT_APP_COVID_BACKEND_URL}/api/countries`;
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const fetchCountries = async (setContinents) => {
+  try {
+    const response = await fetch(url);
+
+    const responseJSON = await response.json();
+
+    setContinents(responseJSON);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default function Home() {
-  const { data: result, error } = useSWR(url, fetcher);
+  const [continents, setContinents] = useState([]);
 
-  if (error) {
-    return <h1>Something went wrong!</h1>;
-  }
+  useEffect(() => {
+    fetchCountries(setContinents);
+  }, []);
 
-  if (!result) {
+  if (!(continents.length > 0)) {
     return <Spinner />;
   }
 
   return (
-    <Grid flex="1">
-      <Accordion>
-        {result.map((continent) => (
-          <AccordionItem>
-            <AccordionHeader>
-              <Box flex="1" textAlign="left">
-                {continent.name}
-              </Box>
-              <AccordionIcon />
-            </AccordionHeader>
-            <AccordionPanel pb={4}>
-              <Grid
-                gridTemplateColumns="repeat(auto-fit, minmax(280px, 1fr))"
-                gridGap={5}
-              >
-                {continent.countries.map((country) => (
-                  <Box shadow="md">
-                    <Text>{country.country}</Text>
-                    <Text>{formatNumber(country.cases.total)}</Text>
-                    <Text>{country.last_update}</Text>
-                  </Box>
-                ))}
-              </Grid>
-            </AccordionPanel>
-          </AccordionItem>
-        ))}
-      </Accordion>
-    </Grid>
+    <Stack flex="1">
+      <SearchInput />
+      <Grid>
+        <Accordion>
+          {continents.map((continent) => (
+            <AccordionItem>
+              <AccordionHeader>
+                <Box flex="1" textAlign="left">
+                  {continent.name}
+                </Box>
+                <AccordionIcon />
+              </AccordionHeader>
+              <AccordionPanel pb={4}>
+                <Grid
+                  gridTemplateColumns="repeat(auto-fit, minmax(280px, 1fr))"
+                  gridGap={5}
+                >
+                  {continent.countries.map((country) => (
+                    <Box shadow="md">
+                      <Text>{country.name}</Text>
+                      <Text>{formatNumber(country.cases.total)}</Text>
+                      <Text>{country.last_update}</Text>
+                    </Box>
+                  ))}
+                </Grid>
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </Grid>
+    </Stack>
   );
-}
-
-function formatNumber(num) {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
