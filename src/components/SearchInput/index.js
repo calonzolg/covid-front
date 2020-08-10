dfb94229ad4b87d2ba156ae66eae4a42d062ad40
Carlos Alonzo Lagos <calonzolg@gmail.com>
2020-08-10 16:42:58 -0600

    refactors to search input component

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,10 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { Input, FormControl, FormLabel, Box, Text } from '@chakra-ui/core';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Box,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Spinner,
+  useDisclosure,
+  PseudoBox,
+} from '@chakra-ui/core';
+import { For } from 'react-loops';
 
 const searchUrl = `${process.env.REACT_APP_COVID_BACKEND_URL}/api/search`;
 
-const searchCountry = async (searchParam, setSearchResult) => {
+const searchCountry = async (
+  searchParam,
+  setSearchResult,
+  onOpen,
+  setIsLoading
+) => {
   try {
     const response = await fetch(searchUrl, {
       method: 'POST',
@@ -21,6 +35,9 @@ const searchCountry = async (searchParam, setSearchResult) => {
         return continent.countries.length > 0;
       })
     );
+
+    onOpen();
+    setIsLoading(false);
   } catch (error) {
     console.log(error);
   }
@@ -42,8 +59,24 @@ function useDebounce(value, delay) {
 
 export default function SearchInput() {
   const [searchText, setSearchText] = useState('');
-  const [searchResult, setSearchResult] = useState(undefined);
+  const [searchResult, setSearchResult] = useState([]);
   const debounceValue = useDebounce(searchText, 500);
+  const [isLoading, setIsLoading] = useState(false);
+  const ref = useRef();
+  const { onClose, isOpen, onOpen } = useDisclosure();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClose();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, onClose]);
 
   function handleChange({ target: { value } }) {
     setSearchText(value);
@@ -51,33 +84,72 @@ export default function SearchInput() {
 
   useEffect(() => {
     if (debounceValue) {
-      searchCountry(debounceValue, setSearchResult);
+      setIsLoading(true);
+      searchCountry(debounceValue, setSearchResult, onOpen, setIsLoading);
     }
-  }, [debounceValue]);
+  }, [debounceValue, onOpen]);
 
   return (
-    <FormControl mb={3}>
-      <FormLabel>Search</FormLabel>
-      <Input
-        placeholder="Search a country"
-        value={searchText}
-        onChange={handleChange}
-      />
-      {searchResult &&
-        (searchResult.length > 0 ? (
-          searchResult.map((continent) => (
-            <Box>
-              <Text>{continent.name}</Text>
-              {continent.countries.map((country) => (
-                <Box as={Link} to={`/country/${country.slug}`}>
-                  {country.name}
-                </Box>
-              ))}
+    <>
+      <InputGroup position="relative">
+        <Input
+          placeholder="Search a country"
+          value={searchText}
+          onChange={handleChange}
+        />
+        <InputRightElement>{isLoading && <Spinner />}</InputRightElement>
+      </InputGroup>
+
+      {isOpen && (
+        <PseudoBox
+          ref={ref}
+          position="absolute"
+          top={['100px', '60px']}
+          overflow="hidden"
+          zIndex={1}
+          width={['280px', '400px']}
+          maxH={['280px', '400px']}
+          bg="white"
+          border="1px solid"
+          borderColor="gray.100"
+          borderRadius="md"
+        >
+          {searchResult.length > 0 ? (
+            <Box overflowY="auto" maxH={['280px', '400px']}>
+              <For
+                of={searchResult}
+                as={(continent) => (
+                  <>
+                    <Box as="ul" bg="gray.400" p={2}>
+                      {continent.name}
+                    </Box>
+                    <For
+                      of={continent.countries}
+                      as={(country, { key }) => (
+                        <Link key={key} to={`/country/${country.slug}`}>
+                          <PseudoBox
+                            as="li"
+                            onClick={onClose}
+                            p={2}
+                            _hover={{
+                              backgroundColor: 'blue.500',
+                              color: 'white',
+                            }}
+                          >
+                            {country.name}
+                          </PseudoBox>
+                        </Link>
+                      )}
+                    />
+                  </>
+                )}
+              />
             </Box>
-          ))
-        ) : (
-          <Box>There is not country found</Box>
-        ))}
-    </FormControl>
+          ) : (
+            <Box>Not country found!</Box>
+          )}
+        </PseudoBox>
+      )}
+    </>
   );
 }
