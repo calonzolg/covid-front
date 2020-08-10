e81f9833dca47ede2c5fda9195c2e59c151fcdb7
Carlos Alonzo Lagos <calonzolg@gmail.com>
2020-08-10 16:44:37 -0600

    add some styles to 404 page 👨‍💻

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import { Box } from '@chakra-ui/core';
+import { Flex, Text } from '@chakra-ui/core';
+import { Link } from 'react-router-dom';
+import Layout from 'components/Layout';
 
 export default function NotFound() {
-  return <Box>not found url</Box>;
+  return (
+    <Layout>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        minH="calc(100vh - 48px)"
+      >
+        <Text fontWeight="bold" fontSize="5xl">
+          404
+        </Text>
+        <Text fontWeight="bold">Not Found url</Text>
+        <Link to="/">got to home</Link>
+      </Flex>
+    </Layout>
+  );
 }
